50ccc5360e8ad05ddae6a2df6e922008f25c7126
Sameer Naik <sameer@damagehead.com>
2013-10-05 23:07:43 +0530

    allow root login from 172.17.%.%'

--- resources/commands.sql
+++ resources/commands.sql
@@ -3,5 +3,6 @@
 -- GRANT ALL ON DATABASE.* to 'db_user'@'%'
 
 -- setup password for root user
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY 'root123' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;
+GRANT ALL ON *.* TO 'root'@'172.17.%.%' IDENTIFIED BY '' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
