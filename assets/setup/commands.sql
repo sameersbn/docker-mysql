4e246e023e56f0013743521e9eb3750278c07838
Sameer Naik <sameer@damagehead.com>
2014-04-06 23:18:11 +0530

    removed commands.sql

--- assets/setup/commands.sql
+++
@@ -1,8 +0,0 @@
--- CREATE DATABASE db_name;
--- CREATE USER 'db_user'@'%' IDENTIFIED BY 'db_password';
--- GRANT ALL ON DATABASE.* to 'db_user'@'%'
-
--- setup password for root user
-GRANT ALL ON *.* TO 'root'@'localhost' IDENTIFIED BY '' WITH GRANT OPTION;
-GRANT ALL ON *.* TO 'root'@'172.17.%.%' IDENTIFIED BY '' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
